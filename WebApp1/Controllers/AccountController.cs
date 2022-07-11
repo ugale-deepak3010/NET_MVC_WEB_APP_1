f7b2a230e1de5ba41d53d0f3d51fcc233fa2c190
Deepak Ugale <65403702+ugale-deepak3010@users.noreply.github.com>
2022-07-11 12:28:08 +0530

    Uploading 2

---
+++ WebApp1/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginX(string UserNamex, string Passwordx)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)     // model bind
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp1/CustomHelper/CustomHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp1.CustomHelper
+{
+    public static class CustomHelper
+    {
+        public static MvcHtmlString SubmitButon(this HtmlHelper htmlHelper, string name, object value)
+        {
+            string str = "<input type='submit' value='"+value+"' name='"+name+"' />";
+            return new MvcHtmlString(str);
+        }
+
+        public static string strSubmitButon(this HtmlHelper htmlHelper, string name, object value)
+        {
+            return "<input type='submit' value='" + value + "' name='" + name + "' />";
+        }
+    }
+}
